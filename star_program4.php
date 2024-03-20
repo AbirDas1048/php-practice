df0b291a09abe4156099d3193cbff9fd7fd21082
Abir Das <abirdas422@gmail.com>
2024-03-20 22:24:51 +0600

    star program 4 file updated

--- star_program4.php
+++ star_program4.php
@@ -1,12 +1,31 @@
 <?php
 
 // Inverted Mirrored Right Triangle Star Pattern
+
+/*
+    Solution 1
+*/
 $n = $m = 10;
 for ($i = 0; $i < $n; $i++) {
     for ($j = $i; $j > 0; $j--) {
         printf(" ");
     }
     for ($k = $m; $k > 0; $k--) {
+        printf("*");
+    }
+    $m--;
+    echo PHP_EOL;
+}
+
+/*
+    Solution 2
+*/
+$n = $m = 10;
+for ($i = 0; $i < $n; $i++) {
+    for ($j = 0; $j < $i; $j++) {
+        printf(" ");
+    }
+    for ($k = 0; $k < $m; $k++) {
         printf("*");
     }
     $m--;
