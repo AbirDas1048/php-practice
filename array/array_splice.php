62b7b83c50b5799144f022fd224f072ebfbcc3f2
Abir Das <abirdas422@gmail.com>
2024-03-26 22:44:06 +0600

    array splice file update

--- array/array_splice.php
+++ array/array_splice.php
@@ -16,7 +16,6 @@ $friends = ["joy", "pranta", "ashu", "sanjoy", "abhishek", "rony", "srejon"];
 // $removed_friends_with_length = array_splice($friends, 2, 2);
 // print_r($removed_friends_with_length);
 
-
 // $removed_friends_negative_index = array_splice($friends, -2);
 // print_r($removed_friends_negative_index);
 
@@ -31,3 +30,49 @@ $friends = ["joy", "pranta", "ashu", "sanjoy", "abhishek", "rony", "srejon"];
 // print_r($removed_friends_with_length_and_add_new_data);
 
 print_r($friends);
+
+
+/*
+    For associative array
+*/
+
+$all_friends = [
+    "joy" => 1014,
+    "pranta" => 1018,
+    "ashu" => 1024,
+    "sanjoy" => 1022,
+    "abhishek" => 1046,
+    "rony" => 1062,
+    "srejon" => 1061,
+];
+
+// $removed_all_friends = array_splice($all_friends, 2);
+// print_r($removed_all_friends);
+
+// $removed_all_friends_with_length = array_splice($all_friends, 2, 2);
+// print_r($removed_all_friends_with_length);
+
+// *** changes when we want to remove and add new data by array_splice for associative array
+
+$new_all_friends = [
+    "rosy" => 1013,
+    "riya" => 1010,
+    "hima" => 1015,
+    "anuradha" => 1052,
+];
+
+/*
+    $removed_all_friends_with_length_and_add_new_data = array_splice($all_friends, 2, 2, $new_all_friends); // This will not give a best result.
+
+    print_r($removed_all_friends_with_length_and_add_new_data);
+*/
+
+/*
+    $all_friends1 = array_slice($all_friends, 0, 2);
+    $all_friends2 = array_slice($all_friends, 4);
+    $all_friends3 = $all_friends1 + $new_all_friends + $all_friends2;
+
+    print_r($all_friends3);
+*/
+
+print_r($all_friends);
