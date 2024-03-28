5ff6adc5df368a71bb6c1436cd38e2a55ea6b79d
Abir Das <abirdas422@gmail.com>
2024-03-28 22:25:30 +0600

    array utility file added

---
+++ array/array_utility.php
@@ -0,0 +1,35 @@
+<?php
+
+$numbers_array = [1, 2, 3, 4, 5, 6, 7];
+
+function powerWithPrint($n, $key, $power)
+{
+    printf("The value of {$n} to the power {$power} is %s\n", $n ** $power);
+}
+
+function powerWithReturn($n)
+{
+    $power = 3;
+    return $n ** $power;
+}
+
+function odd($n)
+{
+    return $n % 2 == 1;
+}
+
+function even($n)
+{
+    return $n % 2 == 0;
+}
+
+array_walk($numbers_array, 'powerWithPrint', 2);
+
+$power_array = array_map('powerWithReturn', $numbers_array);
+print_r($power_array);
+
+$odd_array = array_filter($numbers_array, 'odd');
+print_r($odd_array);
+
+$even_array = array_filter($numbers_array, 'even');
+print_r($even_array);
