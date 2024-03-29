253163fd7741b3bd7d2dd8a5d1037d73aefe686e
Abir Das <abirdas422@gmail.com>
2024-03-29 22:01:39 +0600

    list file added

---
+++ array/list.php
@@ -0,0 +1,8 @@
+<?php
+
+$info = ['Abir', 'Das', 'abir...@gmail.com', 'Software Engineer', 'B.Sc in CSE'];
+
+
+list($first_name, $last_name, $email, $designation, $education) = $info;
+
+printf("{$first_name} {$last_name} is a {$designation}");
