91f1e05fff499263bd58ef9b4c91963048195376
Abir Das <abirdas422@gmail.com>
2024-03-29 22:51:04 +0600

    array rand added

--- array/array_shuffle.php
+++ array/array_shuffle.php
@@ -3,9 +3,11 @@
 $number = range(11, 20);
 print_r($number);
 
+echo array_rand($number) . "\n"; // It will return a random key from $number array.
+
 $random_number = mt_rand(0, 9);
 
 echo $number[$random_number] . "\n";
 
-shuffle($number);
+shuffle($number); // It will shuffle the whole array.
 print_r($number);
