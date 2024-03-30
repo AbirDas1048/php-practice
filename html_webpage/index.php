962b7219837b8159a02e9404be46b7d059717174
Abir Das <abirdas422@gmail.com>
2024-03-30 12:44:04 +0600

    html webpage setup done

---
+++ html_webpage/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css">
+</head>
+<style>
+    body {
+        margin-top: 30px;
+    }
+</style>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="column column-60 column-offset-20">
+                <h1>Hello World</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit enim tempora laboriosam voluptatibus voluptatum. Blanditiis omnis reprehenderit quibusdam adipisci doloribus?</p>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
