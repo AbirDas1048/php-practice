abaffe9a39b49a0a9a033fb5225d88ac2351b41c
Abir Das <abirdas422@gmail.com>
2024-04-05 23:03:00 +0600

    ascii file added

---
+++ string/ascii.php
@@ -0,0 +1,34 @@
+<?php
+
+echo ord('A') . "\n";
+echo ord('B') . "\n";
+echo ord('Y') . "\n";
+echo ord('Z') . "\n";
+echo "\n";
+echo ord('a') . "\n";
+echo ord('b') . "\n";
+echo ord('y') . "\n";
+echo ord('z') . "\n";
+echo "\n";
+echo ord('0') . "\n";
+echo ord('1') . "\n";
+echo ord('8') . "\n";
+echo ord('9') . "\n";
+echo "\n";
+echo ord('!') . "\n";
+echo ord('@') . "\n";
+echo ord('#') . "\n";
+echo ord('$') . "\n";
+echo ord('%') . "\n";
+echo ord('^') . "\n";
+echo ord('*') . "\n";
+echo ord('(') . "\n";
+echo ord(')') . "\n";
+echo ord('-') . "\n";
+echo ord('_') . "\n";
+echo ord('+') . "\n";
+echo ord('=') . "\n";
+echo ord('{') . "\n";
+echo ord('}') . "\n";
+echo ord('[') . "\n";
+echo ord(']') . "\n";
