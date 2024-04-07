e284637330851be24e6e99fb2dfe1bffdfc7a017
Abir Das <abirdas422@gmail.com>
2024-04-07 21:56:49 +0600

    string tokenize file updated

--- string/string tokenize.php
+++ string/string tokenize.php
@@ -1,6 +1,7 @@
 <?php
 
 $string = "The quick brown fox jumps over the lazy dog";
+$string2 = "Loves to travel,coding,learning";
 
 $parts = explode(" ", $string);
 
@@ -10,4 +11,18 @@ echo PHP_EOL;
 $original = implode(" ", $parts);
 
 print_r($original);
+echo PHP_EOL;
+
+$alphabet = str_split($string);
+print_r($alphabet);
+echo PHP_EOL;
+
+$token = strtok($string, " ");
+while ($token !== false) {
+    echo $token . "\n";
+    $token = strtok(" ");
+}
+
+$parts_by_regex = preg_split("/ |,/", $string2);
+print_r($parts_by_regex);
 echo PHP_EOL;
