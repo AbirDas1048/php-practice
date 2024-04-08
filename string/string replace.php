278384f18123a612fd1a85cdda5ed1706f4f1d34
Abir Das <abirdas422@gmail.com>
2024-04-08 17:27:50 +0600

    string replace for multiple data

--- string/string replace.php
+++ string/string replace.php
@@ -15,3 +15,11 @@ echo $replaced3 . PHP_EOL;
 $replaced4 = str_ireplace('the', 'a', $string, $count2); // Case insensitive replace with count.
 echo $replaced4 . PHP_EOL;
 echo "Total replace: {$count2}" . PHP_EOL;
+
+$replaced5 = str_replace(array('the', 'fox', 'dog'), array('a', 'cat', 'hen'), $string, $count3); //Multiple replace case sensitive with count.
+echo $replaced5 . PHP_EOL;
+echo "Total replace: {$count3}" . PHP_EOL;
+
+$replaced6 = str_ireplace(array('the', 'fox', 'dog'), array('a', 'cat', 'hen'), $string, $count4); //Multiple replace case insensitive with count.
+echo $replaced6 . PHP_EOL;
+echo "Total replace: {$count4}" . PHP_EOL;
