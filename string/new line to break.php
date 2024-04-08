ec3903443a4e0de1a9ca1ab6731ba3d676dd6a03
Abir Das <abirdas422@gmail.com>
2024-04-08 17:48:23 +0600

    new line to break file added

---
+++ string/new line to break.php
@@ -0,0 +1,6 @@
+<?php
+
+$string = "Lorem ipsum dolor sit amet consectetur, adipisicing elit.\nMaiores, exercitationem recusandae? Earum commodi sapiente hic nostrum ab voluptate sed maxime? Veniam consequatur sed minima ad tempore ab at quas voluptatum sequi harum, expedita adipisci perspiciatis eius provident explicabo! Dolore, dicta pariatur.\nBeatae eum fugit ab maxime repudiandae nam aspernatur odit perspiciatis itaque soluta a deserunt voluptatum esse aliquam quis facilis hic accusantium voluptate blanditiis minima, veritatis voluptates consectetur qui corporis.\nQuia dolorem error modi odit doloremque, ut facere aut, repellat at tempora alias ab magnam iusto deserunt expedita quisquam illum laborum, distinctio excepturi consequatur.\nAb quod explicabo omnis accusantium voluptate!";
+
+echo $string . PHP_EOL . PHP_EOL;
+echo nl2br($string); // <br /> is used for break in html web page
