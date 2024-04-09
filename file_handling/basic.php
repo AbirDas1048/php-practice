75a79c3d36095a8d1301b1acfbb04c0f657eeaaf
Abir Das <abirdas422@gmail.com>
2024-04-09 22:06:10 +0600

    check is file readable or not

--- file_handling/basic.php
+++ file_handling/basic.php
@@ -1,50 +1,53 @@
 <?php
 
 $file_name = getcwd() . "/file_handling/data/friend.txt";
-$fp = fopen($file_name, 'r');
-$line = fgets($fp);
-echo $line;
-$line = fgets($fp);
-echo $line;
-$line = fgets($fp);
-echo $line;
-$line = fgets($fp, 4); // Limit to read the number of character
-echo $line;
+if (is_readable($file_name)) {
 
-echo PHP_EOL;
+    $fp = fopen($file_name, 'r');
+    $line = fgets($fp);
+    echo $line;
+    $line = fgets($fp);
+    echo $line;
+    $line = fgets($fp);
+    echo $line;
+    $line = fgets($fp, 4); // Limit to read the number of character
+    echo $line;
 
-rewind($fp); // move the cursor to first of the file
+    echo PHP_EOL;
 
-// get all the line
-while ($line2 = fgets($fp)) {
-    echo $line2;
+    rewind($fp); // move the cursor to first of the file
+
+    // get all the line
+    while ($line2 = fgets($fp)) {
+        echo $line2;
+    }
+
+    echo PHP_EOL;
+
+    fseek($fp, 14); // move the cursor in a specific point of the file
+
+    // get all the line
+    while ($line3 = fgets($fp, 5)) {
+        echo $line3 . '-';
+    }
+
+    echo PHP_EOL;
+
+    fseek($fp, -1, SEEK_END);
+    $line4 = fgets($fp); // Nothing will get. because cursor is end of the file
+    echo $line4 . "After print line4";
+
+    echo PHP_EOL;
+
+    $data = file($file_name); // It will return all line in an array of a file.
+    print_r($data);
+
+    echo PHP_EOL;
+
+    $content = file_get_contents($file_name); // It will return all content of a file.
+    echo $content;
+
+    echo PHP_EOL;
+
+    fclose($fp);
 }
-
-echo PHP_EOL;
-
-fseek($fp, 14); // move the cursor in a specific point of the file
-
-// get all the line
-while ($line3 = fgets($fp, 5)) {
-    echo $line3 . '-';
-}
-
-echo PHP_EOL;
-
-fseek($fp, -1, SEEK_END);
-$line4 = fgets($fp); // Nothing will get. because cursor is end of the file
-echo $line4 . "After print line4";
-
-echo PHP_EOL;
-
-$data = file($file_name); // It will return all line in an array of a file.
-print_r($data);
-
-echo PHP_EOL;
-
-$content = file_get_contents($file_name); // It will return all content of a file.
-echo $content;
-
-echo PHP_EOL;
-
-fclose($fp);
