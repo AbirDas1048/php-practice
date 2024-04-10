4490b41554e68003c1fdb53f1cf07bf0a3bc5c53
Abir Das <abirdas422@gmail.com>
2024-04-10 20:58:07 +0600

    file write with previos data example 1 added

--- file_handling/file_write.php
+++ file_handling/file_write.php
@@ -8,3 +8,22 @@ fwrite($fp, "Nishat\n");
 fwrite($fp, "Rafi\n");
 
 fclose($fp);
+
+/*
+    It always remove the previous data from the file.
+
+    To keep the previous data from the file.
+*/
+
+// Example 1
+
+$previous_data = file_get_contents($file_name); // It gets the all previous data from the file
+
+$fp = fopen($file_name, 'w');
+
+fwrite($fp, $previous_data); // First it write the previous data on the file
+fwrite($fp, "Joy\n");
+fwrite($fp, "Pranta\n");
+fwrite($fp, "Ashu\n");
+
+fclose($fp);
