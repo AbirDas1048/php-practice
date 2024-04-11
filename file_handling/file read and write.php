5f468221698a907b181fc88a4238705890297e32
Abir Das <abirdas422@gmail.com>
2024-04-11 14:22:30 +0600

    add last on the file on read write mood

--- file_handling/file read and write.php
+++ file_handling/file read and write.php
@@ -2,6 +2,8 @@
 
 $file_name = getcwd() . "/file_handling/data/friend2.txt";
 
+// Example 1
+
 $fp = fopen($file_name, 'r+');
 
 $line = fgets($fp);
@@ -11,5 +13,15 @@ fwrite($fp, "Bashar\n");
 
 $line = fgets($fp);
 echo $line;
+
+fclose($fp);
+
+// Example 2
+
+$fp = fopen($file_name, 'r+');
+
+fseek($fp, -1, SEEK_END); // Move the cursor last of the file
+
+fwrite($fp, "\nSakib\n");
 
 fclose($fp);
