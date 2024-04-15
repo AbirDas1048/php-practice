016dc5597c0d498ed73d4c2f22dc914347685417
Abir Das <abirdas422@gmail.com>
2024-04-15 23:32:07 +0600

    add new data as serialize data

--- file_handling/file serialize.php
+++ file_handling/file serialize.php
@@ -35,3 +35,18 @@ print_r($export_data);
 unset($export_data[1]);
 $new_data = serialize($export_data);
 file_put_contents($file_name, $new_data, LOCK_EX);
+
+// add new data as serialized data
+
+$new_info = [
+    [
+        'first_name' => "Sanjoy",
+        'last_name' => "Biswas",
+        'id' => "1022",
+        'email' => "sanjoy@gmail.com",
+    ]
+];
+
+array_push($infos, $new_info);
+$new_info_data = serialize($infos);
+file_put_contents($file_name, $new_info_data, LOCK_EX);
