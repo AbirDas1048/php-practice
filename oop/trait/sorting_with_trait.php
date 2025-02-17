0780b12f570fb52aa6557f304a5668e24bc62bac
Abir Das <abirdas422@gmail.com>
2025-02-17 23:26:48 +0600

    HTML added in Sorting with trait

--- oop/trait/sorting_with_trait.php
+++ oop/trait/sorting_with_trait.php
@@ -42,24 +42,23 @@ class Product {
     public $data = [];
     use SortStrategy;
     public function get() {
-        // do something to get the data, for this ex. I just included an array
         $this->data = [
-            101222 => ['label' => 'Awesome product', 'price' => 10.50, 'date_added' => '2012-02-01'],
-            101232 => ['label' => 'Not so awesome product', 'price' => 5.20, 'date_added' => '2012-03-20'],
-            101241 => ['label' => 'Pretty neat product', 'price' => 9.65, 'date_added' => '2012-04-15'],
-            101256 => ['label' => 'Freakishly cool product', 'price' => 12.55, 'date_added' => '2012-01-11'],
-            101219 => ['label' => 'Meh product', 'price' => 3.69, 'date_added' => '2012-06-11'],
+            101222 => ['name' => 'Tomato', 'price' => 50, 'created_at' => '2025-02-08'],
+            101232 => ['name' => 'Potato', 'price' => 20, 'created_at' => '2025-02-12'],
+            101241 => ['name' => 'Capsicum', 'price' => 60, 'created_at' => '2025-02-15'],
+            101256 => ['name' => 'Banana', 'price' => 80, 'created_at' => '2025-02-05'],
+            101219 => ['name' => 'Orange', 'price' => 60, 'created_at' => '2025-02-10'],
         ];
     }
     public function sort_by($by = 'price', $type = 'asc') {
         if (!preg_match('/^(asc|desc)$/', $type)) $type = 'asc';
         switch ($by) {
             case 'name':
-                $this->sort_field = 'label';
+                $this->sort_field = 'name';
                 uasort($this->data, array('Product', 'string_'.$type));
                 break;
             case 'date':
-                $this->sort_field = 'date_added';
+                $this->sort_field = 'created_at';
                 uasort($this->data, array('Product', 'date_'.$type));
                 break;
             default:
@@ -73,25 +72,64 @@ $product = new Product();
 $product->get();
 $product->sort_by('date');
 ?>
-<table border="1" width="100%">
-    <tr>
-        <th>SL</th>
-        <th>Label</th>
-        <th>Price</th>
-        <th>Date Added</th>
-    </tr>
-    <?php
-    $i=1;
-    foreach($product->data as $value){
-        extract($value);
-        ?>
-        <tr>
-            <td><?php echo $i++; ?></td>
-            <td><?php echo $label; ?></td>
-            <td><?php echo $price; ?></td>
-            <td><?php echo $date_added; ?></td>
-        </tr>
-        <?php
-    }
-    ?>
-</table>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sorting with Trait</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css">
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+        }
+
+        body {
+            margin-top: 30px;
+        }
+
+        h2{
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="column column-60 column-offset-20">
+            <h2>Sorting with Trait</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>SL No</th>
+                        <th>Product Name</th>
+                        <th>Product Price</th>
+                        <th>Created at</th>
+                    </tr>
+                </thead>
+                <tbody>
+                <?php
+                $i=1;
+                foreach($product->data as $value){
+                    ?>
+                    <tr>
+                        <td><?php echo $i++; ?></td>
+                        <td><?php echo $value['name']; ?></td>
+                        <td><?php echo $value['price']; ?></td>
+                        <td><?php echo $value['created_at']; ?></td>
+                    </tr>
+                    <?php
+                }
+                ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</body>
+
+</html>
