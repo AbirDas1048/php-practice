51a277ddcad4c38273aa2b672f53a2d6fc093f63
Abir Das <abirdas422@gmail.com>
2025-02-20 00:36:39 +0600

    prevent class inherit added

--- oop/prevent_method_overriding.php
+++ oop/prevent_method_overriding.php
@@ -21,7 +21,8 @@ class Admin extends Member{
     /***
      * This will return fatal error.
      * Because in parent class logIn method declare as final keyword.
-     * For final keyword we can't override a method
+     * For final keyword method we can't override a method
+     * Fatal error: Cannot override final method Member::logIn()
      */
     public function logIn(): void
     {
@@ -38,3 +39,22 @@ $admin = new Admin();
 $admin->user_name = "Abir";
 $admin->logIn();
 $admin->logOut();
+
+
+final class Exam
+{
+    // This class can't be extended at all
+}
+
+/***
+ * prevent class Inheritance
+ * This will return fatal error.
+ * Because Exam class declare as final keyword.
+ * For final keyword class we can't inherit to another class
+ * Fatal error: Class Quiz cannot extend final class Exam
+ */
+
+//class Quiz extends Exam
+//{
+//// Will show Fatal Error
+//}
