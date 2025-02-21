79355a79dc289a40f72015112dfa5e16e8ae4398
Abir Das <abirdas422@gmail.com>
2025-02-21 23:21:07 +0600

    Total Calculation added

--- oop/create_mathematical_operation_with_static_property_and_method.php
+++ oop/create_mathematical_operation_with_static_property_and_method.php
@@ -1,13 +1,23 @@
 <?php
 class Mathematical {
+
+    private static $total_operation = 0;
+
     public static function add($a, $b)
     {
+        self::$total_operation++;
         return $a + $b;
     }
 
     public static function subtract($a, $b)
     {
+        self::$total_operation++;
         return $a - $b;
+    }
+
+    public static function getTotalOperation()
+    {
+        return self::$total_operation;
     }
 }
 
@@ -16,3 +26,4 @@ $subtract_result = Mathematical::subtract(10, 5);
 
 echo "Add result is: $add_result\n";
 echo "Subtract result is: $subtract_result\n";
+echo "Total operation count is: " . Mathematical::getTotalOperation() . PHP_EOL;
