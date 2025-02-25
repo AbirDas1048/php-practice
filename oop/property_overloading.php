e0700e682e945cddefc637c4f629bf9708bee93b
Abir Das <abirdas422@gmail.com>
2025-02-26 00:43:03 +0600

    How to use property overloading

--- oop/property_overloading.php
+++ oop/property_overloading.php
@@ -11,21 +11,55 @@ class Overloading{
     public function __set($key, $value)
     {
         $this->data[$key] = $value;
-        echo "$key is set\n";
+        echo "$key property is set\n";
     }
 
     public function __isset($key){
-        echo "Checking the $key is set or not\n";
+        echo "Checking the $key property is set or not. ";
         return isset($this->data[$key]);
     }
 
     public function __get($key){
-        echo "Getting the value of $key\n";
+        echo "The value of $key property is ";
         return $this->data[$key];
     }
 
     public function __unset($key){
-        echo "Unset the value of $key\n";
+        echo "Unset the $key property\n";
         unset($this->data[$key]);
     }
+}
+
+$overloading = new Overloading();
+
+$overloading->first_name = 'Abir';
+$overloading->last_name = 'Das';
+$overloading->dob = '31/101995';
+$overloading->designation = 'Software Engineer';
+
+if(isset($overloading->first_name)){
+    echo $overloading->first_name . PHP_EOL;
+}else{
+    echo "Property not set";
+}
+
+if(isset($overloading->father_name)){
+    echo $overloading->father_name . PHP_EOL;
+}else{
+    echo "Property not set\n";
+}
+
+if(isset($overloading->dob)){
+    echo $overloading->dob . PHP_EOL;
+}else{
+    echo "Property not set\n";
+}
+
+unset($overloading->dob);
+// echo $overloading->dob; // It will give error. Because dob property is unset.
+
+if(isset($overloading->dob)){
+    echo $overloading->dob . PHP_EOL;
+}else{
+    echo "Property not set\n";
 }
