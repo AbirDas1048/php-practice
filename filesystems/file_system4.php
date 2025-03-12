29aeb4e7488eebede22a548efa1dc54be23d1dbe
Abir Das <abirdas422@gmail.com>
2025-03-13 00:16:41 +0600

    create file function added

--- filesystems/file_system4.php
+++ filesystems/file_system4.php
@@ -6,6 +6,12 @@
 
 // For create a single directory
 mkdir('test_file_system');
+sleep(2);
 
 // For create a recursive directory. Directory inside directory
 mkdir('test_file_system2/t1/t2/t3/t4/t5', 0777, true);
+sleep(2);
+
+// For create a file with content in a specific directory
+file_put_contents('test_file_system2/t1/t2/test.txt', 'test');
+sleep(2);
