d28952ced7e91ece05b90359b48132e458842dab
Abir Das <abirdas422@gmail.com>
2025-03-14 00:02:03 +0600

    remove directory with an unlinked file added

--- filesystems/file_system4.php
+++ filesystems/file_system4.php
@@ -15,3 +15,15 @@ sleep(2);
 // For create a file with content in a specific directory
 file_put_contents('test_file_system2/t1/t2/test.txt', 'test');
 sleep(2);
+
+file_put_contents('test_file_system2/t1/t2/t3/t4/t5/test.txt', 'test');
+sleep(2);
+
+/***
+ * For remove directory we use rmdir().
+ * But if it's not empty, it will throw error.
+ * So we need to unlink the file.
+*/
+unlink('test_file_system2/t1/t2/t3/t4/t5/test.txt');
+sleep(2);
+rmdir('test_file_system2/t1/t2/t3/t4/t5');
