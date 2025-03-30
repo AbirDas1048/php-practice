7f4b441cae4892f86b7af771194ae3841ca75e83
Abir Das <abirdas422@gmail.com>
2025-03-31 00:18:22 +0600

    SOLID Principle start

---
+++ oop/advance/SOLID/index.php
@@ -0,0 +1,10 @@
+<?php
+/***
+ * The SOLID principles are a set of design principles in object-oriented programming that help create maintainable, scalable, and testable software.
+ * SOLID principles were introduced by Robert C. Martin in his 2000 paper “Design Principles and Design Patterns.”
+ *
+ * Why do Design Principles matter?
+ * Encourage us to create more maintainable, understandable, and flexible software.
+ * As applications grow in size, we can reduce their complexity and save ourselves a lot of headaches.
+ * Software is modular, easy to understand, debug, and refactor.
+ */
