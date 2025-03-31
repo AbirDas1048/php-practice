4809452e1f156468ac111e7c5f6d74e79c4b2be7
Abir Das <abirdas422@gmail.com>
2025-04-01 00:07:04 +0600

    pillars of SOLID Principle added

--- oop/advance/SOLID/index.php
+++ oop/advance/SOLID/index.php
@@ -7,4 +7,19 @@
  * Encourage us to create more maintainable, understandable, and flexible software.
  * As applications grow in size, we can reduce their complexity and save ourselves a lot of headaches.
  * Software is modular, easy to understand, debug, and refactor.
+ *
+ * Why SOLID?
+ * As a software developer, we always need to deal with changes in requirements or a new feature in the middle of development or some extension on a feature in the maintenance phase.
+ * It is important to write code that’s easy to extend and maintain and make our life less painful.
+ * SOLID is a set of rules that guide us on how to achieve that in object-oriented programming.
+ * Using SOLID guidelines, you can avoid bad design in your code and give a well-structured architecture to your design.
+ * Bad design leads to the inflexible and brittle codebase, a simple and small change can result in bugs with bad design.
+ *
+ * SOLID is an acronym of five sets of principles.
+ * Following are the five concepts that make up SOLID principles:
+ * Single Responsibility principle
+ * Open/Closed principle
+ * Liskov Substitution principle
+ * Interface Segregation principle
+ * Dependency Inversion principle.
  */
