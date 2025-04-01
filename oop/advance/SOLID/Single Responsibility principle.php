9222ac3e5bb25b3f10261e31645f7f42873c66e4
Abir Das <abirdas422@gmail.com>
2025-04-02 00:04:19 +0600

    Single Responsibility Principle definition added

---
+++ oop/advance/SOLID/Single Responsibility principle.php
@@ -0,0 +1,8 @@
+<?php
+/***
+ * Single Responsibility Principle: As the name suggests, this principle’s purpose is to have a single responsibility for a class/module.
+ * In other words, we can say that the class or module should solve one and only one problem.
+ * So it should have a single reason to change.
+ * It makes our code more cohesive, hence makes it easy to test and maintain.
+ * “A class should only have a single responsibility, that is, only changes to one part of the software’s specification should be able to affect the specification of the class. ”
+ */
