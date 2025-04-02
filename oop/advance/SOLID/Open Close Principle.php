d5aea6a40a49aef5ef3a2a3714a40c68f030c1f0
Abir Das <abirdas422@gmail.com>
2025-04-03 00:18:19 +0600

    Open/Close Principle definition added

---
+++ oop/advance/SOLID/Open Close Principle.php
@@ -0,0 +1,9 @@
+<?php
+/***
+ * Open/Close Principle: This principal purpose that existed and well-tested class should not be modified when a new feature needs to be built.
+ * It may introduce a new bug when we modify an existing class to make a new feature.
+ * So rather than changing an existing class/Interface, we should extend that class/Interface in a new class to add new features.
+ * The Open/Closed Principle emphasizes that classes should be open for extension but closed for modification.
+ * In simpler terms, we should be able to extend the behavior of a class without modifying its existing code.
+ * This principle promotes code reuse and makes it easier to add new features.
+ */
