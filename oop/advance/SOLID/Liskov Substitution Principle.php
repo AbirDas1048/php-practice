51223fe683567109fe23517aa4a05b1c639a218d
Abir Das <abirdas422@gmail.com>
2025-04-04 00:09:19 +0600

    Liskov Substitution Principle definition added

---
+++ oop/advance/SOLID/Liskov Substitution Principle.php
@@ -0,0 +1,10 @@
+<?php
+/***
+ * The Liskov Substitution Principle states that objects of a superclass should be replaceable with objects of its subclasses without affecting the correctness of the program.
+ * In simpler terms, derived classes should be able to replace their base classes without introducing errors.
+ * This principle is named after the name of Barbara Liskov.
+ * She introduced this principle in 1987.
+ * The concept states that a subtype must be substitutable to base types without breaking the behavior.
+ * It is a particular definition of subtyping relation, called behavioral subtyping.
+ * “Objects in a program should be replaceable with instances of their subtypes without altering the correctness of that program.”
+ */
