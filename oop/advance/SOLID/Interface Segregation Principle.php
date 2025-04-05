604568017f4e3263b9e65e1be110a3f29e3095fb
Abir Das <abirdas422@gmail.com>
2025-04-05 22:22:50 +0600

    Revert "Interface Segregation Principle definition added"
    This reverts commit 3b5f1c9771f7db1c9d79c45fb78ce27c6c2ece19.

--- oop/advance/SOLID/Interface Segregation Principle.php
+++
@@ -1,9 +0,0 @@
-<?php
-/***
- * The Interface Segregation Principle suggests that clients should not be forced to depend on interfaces they do not use.
- * In essence, it is better to have multiple specific interfaces than one large, general-purpose interface.
- * This principle states that an interface should not enforce unwanted methods to a class.
- * The idea here is instead of having a large interface, we should have smaller interfaces.
- * So we should not create an interface having a lot of methods If we have such an interface, we should break in into smaller interfaces.
- * “Many client-specific interfaces are better than one general-purpose interface.”
- */
