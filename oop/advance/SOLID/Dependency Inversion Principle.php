1e4a7adec4f81412351877a6f9ae2208fd95f9a1
Abir Das <abirdas422@gmail.com>
2025-04-06 00:05:12 +0600

    Dependency Inversion Principle definition added

---
+++ oop/advance/SOLID/Dependency Inversion Principle.php
@@ -0,0 +1,10 @@
+<?php
+/***
+ * The Dependency Inversion Principle states that high-level modules should not depend on low-level modules; both should depend on abstractions.
+ * In addition, abstractions should not depend on details; details should depend on abstractions.
+ * This principle encourages the use of interfaces and dependency injection to decouple components and improves flexibility.
+ * This principle aims to decouple high-level modules (which provide complex functionality) from low-level modules (which provide basic functionality) by introducing an abstraction layer between them.
+ * This allows both high-level and low-level modules to depend on abstractions rather than on solid implementations, making the system more flexible, extensible, and maintainable.
+ * According to this principle, higher level classes should not directly depend on lower level classes but abstractions.
+ * It means that a higher level class should not need to know the implementation details of the low-level class, the low-level class should be hidden behind an abstraction.
+ */
