693b70fac26ee6515de5d12f24f350e0c5f142d3
Abir Das <abirdas422@gmail.com>
2025-04-21 23:07:55 +0600

    Form Builder separte php form

--- project-form-builder/views/form.php
+++ project-form-builder/views/form.php
@@ -1,1 +1,8 @@
 <?php
+use Abir\FormBuilder\classes\FormBuilder;
+use Abir\FormBuilder\fields\TextField;
+
+$form = new FormBuilder();
+$form->addField(new TextField('username', 'Username'));
+
+echo $form->render();
