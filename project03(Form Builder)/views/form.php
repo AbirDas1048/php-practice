0ede77ff568ec62d655f0b71c5ff6428994b5add
Abir Das <abirdas422@gmail.com>
2025-04-26 23:07:54 +0600

    Form Builder other input field added

--- project03(Form Builder)/views/form.php
+++ project03(Form Builder)/views/form.php
@@ -4,5 +4,28 @@ use Abir\FormBuilder\fields\InputField;
 use Abir\FormBuilder\config\Constants;
 
 $form = new FormBuilder();
-$form->addField(new InputField(Constants::INPUT_TYPE_TEXT, 'name', 'name', 'Name', ['class' => 'from-control', 'required' => true]));
+$form->addField(new InputField(
+    Constants::INPUT_TYPE_TEXT, 'full_name', 'full_name', 'Full Name',
+    ['class' => 'from-control', 'required' => true, 'placeholder' => 'Full Name']
+));
+$form->addField(new InputField(
+    Constants::INPUT_TYPE_TEXT, 'username', 'username', 'Username',
+    ['class' => 'from-control', 'required' => true]
+));
+$form->addField(new InputField(
+    Constants::INPUT_TYPE_EMAIL, 'email', 'email', 'Email',
+    ['class' => 'from-control', 'value' => 'name@email.com', 'readonly' => true]
+));
+$form->addField(new InputField(
+    Constants::INPUT_TYPE_PASSWORD, 'password', 'password', 'Password',
+    ['class' => 'from-control', 'required' => true]
+));
+$form->addField(new InputField(
+    Constants::INPUT_TYPE_NUMBER, 'salary', 'salary', 'Salary',
+    ['class' => 'from-control', 'min' => 0, 'max' => 10000000]
+));
+$form->addField(new InputField(
+    Constants::INPUT_TYPE_HIDDEN, 'id', 'id', '', ['required' => true]
+));
+
 echo $form->render();
