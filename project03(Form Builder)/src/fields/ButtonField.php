b58d098389a4627f27746265ff4550c2e3497c1c
Abir Das <abirdas422@gmail.com>
2025-05-13 23:10:11 +0600

    Form Builer ButtonField constract and render added

--- project03(Form Builder)/src/fields/ButtonField.php
+++ project03(Form Builder)/src/fields/ButtonField.php
@@ -3,12 +3,27 @@
 namespace Abir\FormBuilder\fields;
 
 use Abir\FormBuilder\interfaces\FormFieldInterface;
+use Abir\FormBuilder\traits\FieldAttributeTrait;
 
 class ButtonField implements FormFieldInterface
 {
+    use FieldAttributeTrait;
+
+    protected string $label;
+    protected string $type;
+    public function __construct(string $type, string $label, array $attributes = []){
+        $this->type = $type;
+        $this->label = $label;
+        $this->setAttributes($attributes);
+    }
 
     public function render(): string
     {
-        // TODO: Implement render() method.
+        return sprintf(
+            '<button type="%s"%s>%s</button>',
+            htmlspecialchars($this->type),
+            $this->renderAttributes(),
+            htmlspecialchars($this->label)
+        );
     }
 }
