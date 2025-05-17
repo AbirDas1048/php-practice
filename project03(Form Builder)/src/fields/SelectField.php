32e011c5d1a8c52d42371fa3734d5c1a6bcb31da
Abir Das <abirdas422@gmail.com>
2025-05-17 13:39:53 +0600

    Form Builder select field file added

---
+++ project03(Form Builder)/src/fields/SelectField.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Abir\FormBuilder\fields;
+
+use Abir\FormBuilder\interfaces\FormFieldInterface;
+
+class SelectField implements FormFieldInterface
+{
+
+    public function render(): string
+    {
+        // TODO: Implement render() method.
+    }
+}
