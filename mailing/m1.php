d1353371aaebdd2da942114feeb0d900601b6a0f
Abir Das <abirdas422@gmail.com>
2025-05-23 23:25:54 +0600

    Mailer cc added

--- mailing/m1.php
+++ mailing/m1.php
@@ -1,8 +1,10 @@
 <?php
 
 $to = 'abirdas422@gmail.com';
+$cc = 'abirdas1151@gmail.com';
 $from = 'Abir Office <abir.das@softrobotics.com.bd>';
 $subject = 'Test mail';
 $message = 'Hi,/n This is an test mail';
 $headers = 'From: '.$from."\r\n";
+$headers .= "CC: ".$cc."\r\n";
 echo mail($to,$subject,$message,$headers);
