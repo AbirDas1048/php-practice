0550dcd11bed75ee3cf80c39e59bd68121938c1e
Abir Das <abirdas422@gmail.com>
2025-05-31 22:28:59 +0600

    error log custom log file added

--- error/error_log.php
+++ error/error_log.php
@@ -6,5 +6,11 @@ if (class_exists('IntlTimeZone')) {
     $timezoneId = $tz->getID();
     date_default_timezone_set($timezoneId);
 }
-error_log("This is an error message 4");
+$log_file_destination = "C:/xampp/php/logs/custom_log_file.txt";
 
+if (is_dir(dirname($log_file_destination))) {
+    error_log("Logging to custom file", 3, $log_file_destination);
+    echo "Log written successfully.";
+} else {
+    echo "Log folder does not exist.";
+}
