c01f44e49695739ca42aa9ea200eb214a3497c13
Abir Das <abirdas422@gmail.com>
2025-06-01 22:20:23 +0600

    error log time zone added

--- error/error_log.php
+++ error/error_log.php
@@ -1,4 +1,10 @@
 <?php
 echo ini_get("error_log");
-error_log("This is an error message 2");
+
+if (class_exists('IntlTimeZone')) {
+    $tz = \IntlTimeZone::createDefault();
+    $timezoneId = $tz->getID();
+    date_default_timezone_set($timezoneId);
+}
+error_log("This is an error message 4");
 
