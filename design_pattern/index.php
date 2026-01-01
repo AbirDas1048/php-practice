c158880263169a26ecee90533bc8c810dd1c3bc9
Abir Das <abirdas422@gmail.com>
2026-01-01 13:16:54 +0600

    design pattern index added

---
+++ design_pattern/index.php
@@ -0,0 +1,13 @@
+<?php
+/***
+ * A design pattern is a proven, reusable solution to a commonly occurring problem in software design.
+ * It’s not ready-made code, but a template or guideline for how to structure your code to solve a problem in a clean, flexible, and maintainable way.
+ * Design patterns are typical solutions to commonly occurring problems in software design.
+ * They are like pre-made blueprints that you can customize to solve a recurring design problem in your code.
+ * সফটওয়্যার ইঞ্জিনিয়ারিং এ ডিজাইন প্যাটার্ন হল কোডের সাধারণ সমস্যার রি-ইউজিবিলিটি বাড়ানোর জন্য এক প্রকারের নিয়মনীতি অথবা টেম্পলেট।
+ * যাতে করে সফটওয়্যার একটা নির্দিষ্ট আর্কিটেকচার এ তৈরি করা যায় আর কোডের পুনুরাব্রিত্তি ঠেকান যায়।
+ * The Catalog of Design Patterns
+ * Creational Design Pattern
+ * Structural Design Pattern
+ * Behavioural Design Pattern
+ */
