27e5c46eaf16679a4caffdde0a08ffd9e8ed91da
Abir Das <abirdas422@gmail.com>
2026-01-03 22:04:45 +0600

    Singleton example added

--- design_pattern/singleton.php
+++ design_pattern/singleton.php
@@ -21,3 +21,33 @@
 // * Logger
 // * Cache service
  */
+
+class Singleton{
+    private static $instance;
+    private $name;
+    private function __construct($name){
+        $this->name = $name;
+    }
+    public static function getInstance($name = null): Singleton
+    {
+        if(!self::$instance){
+            echo "Creating New Instance\n";
+            self::$instance = new Singleton($name ?? '');
+        }else{
+            echo "Returning Old Instance\n";
+        }
+        return self::$instance;
+    }
+
+    public function sayName(): void
+    {
+        echo $this->name."\n";
+    }
+}
+
+$obj1 = Singleton::getInstance('Karim');
+$obj2 = Singleton::getInstance('Rahim');
+var_dump($obj1 === $obj2);
+echo PHP_EOL;
+$obj1->sayName();
+$obj2->sayName();
