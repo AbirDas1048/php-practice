f0a32f68a0aad8b9abfb47edb79d572b7cda5790
Abir Das <abirdas422@gmail.com>
2026-01-12 23:01:48 +0600

    Decorator theory added

---
+++ design_pattern/decorator_theory.php
@@ -0,0 +1,34 @@
+<?php
+/***
+ * Decorator Pattern is a Structural Pattern.
+ * Decorator Pattern attach new behaviors to objects by placing these objects inside special wrapper objects that contain the behaviors.
+ * The Decorator pattern lets you add new behavior to an object at runtime without modifying its class.
+ * Decorator = wrap an object to enhance it
+ */
+
+/***
+ * ---> When to use Decorator <---
+ * You want to add features dynamically
+ * You want to avoid subclass explosion
+ * You want to follow Open/Closed Principle
+ * Behavior combinations can vary
+ *
+ *  ---> When NOT to use Singleton <---
+ * Very simple features
+ * When behavior won’t change
+ * When inheritance is sufficient
+ *
+ * --> Common real-world uses <--
+ * Laravel Middleware works the same way:
+ *
+ * $request
+ * → AuthMiddleware
+ * → ThrottleMiddleware
+ * → Controller
+ *
+ *
+ * Each middleware:
+ * wraps request
+ * adds behavior
+ * passes control forward
+ */
